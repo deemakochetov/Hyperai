3e030c7f36de74078e704dbea8d7ad0643cbca55
kochetovdmytro <kochetovdmytro@gmail.com>
2023-03-08 15:14:37 +0100

    add progress bar

--- js/about.js
+++ js/about.js
@@ -3,3 +3,19 @@ const infoText = document.querySelector(".info-text");
 infoIcon.addEventListener("click", function () {
   infoText.classList.toggle("visible");
 });
+
+function moveProgressBar() {
+  let getProgressWrapWidth =
+    document.querySelector(".progress-wrap").offsetWidth;
+  let progressTotal = getProgressWrapWidth;
+  let animationLength = 2500;
+
+  let progressBar = document.getElementById("progress-bar");
+  progressBar.style.left = progressTotal + "px";
+  progressBar.style.color = "red";
+}
+
+const scheduleBtn = document.getElementById("schedule-button");
+scheduleBtn.addEventListener("click", () => {
+  moveProgressBar();
+});
